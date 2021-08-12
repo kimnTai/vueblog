0a0a03a53e1f1c55fdd5d6bb45ec85b1da464ec4
kimn <a9523023650@gmail.com>
2021-08-12 12:32:13 +0800

    202108121231
    上傳前端vue 頁面

---
+++ vueblog-java/src/main/java/com/kimn/controller/BlogController.java
@@ -0,0 +1,74 @@
+package com.kimn.controller;
+
+
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.kimn.common.lang.Result;
+import com.kimn.entity.Blog;
+import com.kimn.service.BlogService;
+import com.kimn.util.ShiroUtil;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author kimn
+ * @since 2021-08-07
+ */
+@RestController
+public class BlogController {
+
+    @Autowired
+    BlogService blogService;
+
+    @GetMapping("/blogs")
+    public Result list(@RequestParam(defaultValue = "1") Integer currenPage) {
+
+        Page page = new Page(currenPage, 5);
+        IPage pageData = blogService.page(page, new QueryWrapper<Blog>().orderByDesc("created"));
+
+        return Result.success(pageData);
+    }
+
+    @GetMapping("/blog/{id}")
+    public Result detail(@PathVariable(name = "id") Long id) {
+        Blog blog = blogService.getById(id);
+        Assert.notNull(blog, "該blog已被刪除");
+
+        return Result.success(blog);
+    }
+
+    @RequiresAuthentication
+    @PostMapping("/blog/edit")
+    public Result edit(@Validated @RequestBody Blog blog) {
+
+        Blog temp = null;
+        if (blog.getId() != null) {
+            temp = blogService.getById(blog.getId());
+            // 只能編輯自己的文章
+            Assert.isTrue(temp.getUserId().equals(ShiroUtil.getProfile().getId()),"沒有權限編輯");
+
+        } else {
+            temp = new Blog();
+            temp.setUserId(ShiroUtil.getProfile().getId());
+            temp.setCreated(LocalDateTime.now());
+            temp.setStatus(0);
+        }
+
+        BeanUtil.copyProperties(blog,temp,"id","userId","created","status");
+        blogService.saveOrUpdate(temp);
+
+
+        return Result.success(null);
+    }
+}
---
+++ vueblog-java/src/main/java/com/kimn/service/UserService.java
@@ -0,0 +1,16 @@
+package com.kimn.service;
+
+import com.kimn.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author kimn
+ * @since 2021-08-07
+ */
+public interface UserService extends IService<User> {
+
+}
